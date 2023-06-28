9e3b448c7fe0210870fbf922a5f0fd611ca4b5f0
Somesh Sharma <sharmsom@amazon.com>
2023-06-29 01:47:21 +0530

    Assignments of Week-5

---
+++ Week-5/Behind scene and JS engine/import_export module/calculator.js
@@ -0,0 +1,13 @@
+// function sayhello(){
+//     console.log("Hello");
+// }
+
+function add(a,b){
+    return `Sum is ${a+b}`;
+}
+
+function subtract(a,b){
+    return `Difference is ${a-b}`;
+}
+
+export {add, subtract }
