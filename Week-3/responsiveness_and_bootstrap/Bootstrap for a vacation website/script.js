44f8d658950e104249268a5aa207b41c154a2bd5
Somuesh <32160906+Somuesh@users.noreply.github.com>
2023-07-10 10:58:53 +0530

    Updated script.js
    Instead of querySelector used getElementById

--- Week-3/responsiveness_and_bootstrap/Bootstrap for a vacation website/script.js
+++ Week-3/responsiveness_and_bootstrap/Bootstrap for a vacation website/script.js
@@ -1,12 +1,12 @@
 function showAlert(alertId) {
-   const bootstrapAlert = document.querySelector("#" + alertId);
+   const bootstrapAlert = document.getElementById(alertId);
    const collapse = new bootstrap.Collapse(bootstrapAlert); 
    collapse.show();
 }
 
 const modalYesBtn = document.querySelector("#contact-modal .btn-primary");
 modalYesBtn.addEventListener("click", function () {    
-   const sendBtn = document.querySelector("#send-message-btn");
+   const sendBtn = document.getElementById("send-message-btn");
    sendBtn.style.display = "none";
    showAlert("conf-alert");
 });
